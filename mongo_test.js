369bfb709470a41cb4e8aa0ab57e1ce8dac67cd8
Farheenkhan1211 <103601940+Farheenkhan1211@users.noreply.github.com>
2023-04-12 23:10:10 +0530

    Update mongo_test.js

--- mongo_test.js
+++ mongo_test.js
@@ -1,38 +1,42 @@
 const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost:27017";
-
+const url =
+  "mongodb+srv://farheenkhan08:GiyDXzBBpb4Jx7S9@farheenkhanbanking.tcrsgun.mongodb.net/?retryWrites=true&w=majority";
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-  console.log("connected!");
+  if (err) {
+    console.log(err, "error");
+  } else {
+    console.log("connected!");
 
-  //db name
-  const dbName = "badbank";
-  const db = client.db(dbName);
+    //db name
+    const dbName = "badbank";
+    const db = client.db(dbName);
 
-  //new user
-  let name = "user" + Math.floor(Math.random() * 1000);
-  let email = name + "@mit.edu";
-  let accountType = "";
-  let balance = 0;
-  const user = {
-    name: name,
-    email: email,
-    accounttype: accountType,
-    accountnumber: 12345678,
-    balance: balance,
-  };
+    //new user
+    let name = "user" + Math.floor(Math.random() * 1000);
+    let email = name + "@mit.edu";
+    let accountType = "";
+    let balance = 0;
+    const user = {
+      name: name,
+      email: email,
+      accounttype: accountType,
+      accountnumber: 12345678,
+      balance: balance
+    };
 
-  //insert into customer collection
-  const collection = db.collection("customers");
-  const doc = { user };
-  collection.insertOne(doc, { w: 1 }, (err, result) => {
-    console.log("Document insert");
-  });
-
-  const customers = db
-    .collection("customers")
-    .find()
-    .toArray((err, docs) => {
-      console.log("Collection:", docs);
-      client.close();
+    //insert into customer collection
+    const collection = db.collection("customers");
+    const doc = { user };
+    collection.insertOne(doc, { w: 1 }, (err, result) => {
+      console.log("Document insert");
     });
+
+    const customers = db
+      .collection("customers")
+      .find()
+      .toArray((err, docs) => {
+        console.log("Collection:", docs);
+        client.close();
+      });
+  }
 });
